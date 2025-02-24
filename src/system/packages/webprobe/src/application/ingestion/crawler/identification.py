fa898546755e8c4812d5591ff590bc30df9e09fa
Cam <cammclain@protonmail.com>
2025-02-23 18:27:31 -0600

    Add identification module to process crawled tech stack information

---
+++ src/system/packages/webprobe/src/application/ingestion/crawler/identification.py
@@ -0,0 +1,7 @@
+
+
+# this file is for using the crawled information to understand the tech stack
+# for example, if the crawler detects a JavaScript file, it should save it for further assessment and identification
+# this could be:
+# identifying the framework used (React, Vue, whatever)
+# the version of the framework or library
